37007cb49f8d35a1f80d820e4bbb03ebd4b0ba4f
John Doe <johndoe@example.com>
2020-08-27 14:56:50 +0800

    dfdfd

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -11,9 +11,10 @@ public class Person {
   public Person() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String person(String someone) {
+  /**
+  *需要添加备注
+  */
+  public final String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
