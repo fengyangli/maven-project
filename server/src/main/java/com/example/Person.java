49ddfab96e53273a719fd0b00c9cb0c7eacb8fb6
John Doe <johndoe@example.com>
2020-08-27 15:14:43 +0800

    123

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -12,7 +12,7 @@ public class Person {
 
   }
   /**
-  *需要添加备注
+  * 需要添加备注
   */
   public final String person(final String someone) {
     return String.format("Hello, %s!", someone);
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,5 +1,5 @@
-package com.example;
-
 /**
 * This is a class.
 */
+
+package com.example;
